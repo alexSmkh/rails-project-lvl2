e5e89050cdf492e78e0206d58ccb7cfa7eac2731
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-02-22 18:16:55 +0300

    Added the authenticate_user! hook to the posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index]
   before_action :set_post, only: %i[show edit update destroy]
+
 
   # GET /posts or /posts.json
   def index
