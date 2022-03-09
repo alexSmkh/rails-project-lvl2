9b20f1938a34abeef3ad0cbe28ff7a86389a033a
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-03-10 01:10:41 +0300

    Removed the auth_user callback before the show action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,21 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index]
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_post, only: %i[show edit update destroy]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.order(created_at: :desc)
   end
 
-  # GET /posts/1 or /posts/1.json
   def show
     @comment = PostComment.new
   end
 
-  # GET /posts/new
   def new
     @post = current_user.posts.build
   end
 
-  # POST /posts or /posts.json
   def create
     @post = current_user.posts.build(post_params)
 
@@ -34,10 +30,8 @@ class PostsController < ApplicationController
     end
   end
 
-  # GET /posts/1/edit
   def edit; end
 
-  # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
       if @post.update(post_params)
@@ -50,7 +44,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
@@ -62,12 +55,10 @@ class PostsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :body, :user_id, :post_category_id)
   end
