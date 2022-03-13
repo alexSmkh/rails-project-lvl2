05e34db846551349efc8dcd0215bb6601a4696e7
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-03-13 23:04:02 +0300

    Added a length validation

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -5,4 +5,13 @@ class PostComment < ApplicationRecord
   belongs_to :post
 
   has_ancestry
+
+  validates :content,
+            presence: true,
+            length: {
+              minimum: 2,
+              maximum: 3000,
+              too_long: '%<count>s characters is the maximum allowed',
+              too_short: '%<count>s characters is the minimum allowed'
+            }
 end
