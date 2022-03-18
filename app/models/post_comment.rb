f8a3203c6b9922a2c3dcf77ea2557f724b35051b
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-03-18 15:33:23 +0300

    Deleted unnecessary error texts for length validation

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -8,10 +8,5 @@ class PostComment < ApplicationRecord
 
   validates :content,
             presence: true,
-            length: {
-              minimum: 2,
-              maximum: 3000,
-              too_long: ->(_, err) { I18n.t('errors.too_long', count: err[:count]) },
-              too_short: ->(_, err) { I18n.t('errors.too_short', count: err[:count]) }
-            }
+            length: { minimum: 2, maximum: 3000 }
 end
