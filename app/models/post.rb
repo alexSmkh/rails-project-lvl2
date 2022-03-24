db9c2d92d2a3c4edb9084cea22fa822c7ab95173
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-03-24 21:59:09 +0300

    Fixed the post body validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,5 +7,5 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :body, presence: true, length: { minumum: 30, maximum: 20_000 }
+  validates :body, presence: true, length: { minumum: 10, maximum: 20_000 }
 end
