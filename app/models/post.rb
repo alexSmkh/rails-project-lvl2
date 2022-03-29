c7552cbae9a78145ef69ef3d15c3f522eb4a44f1
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-03-29 12:23:05 +0300

    Changed the length of the post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,6 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'PostComment', dependent: :destroy
   has_many :likes, class_name: 'PostLike', dependent: :destroy
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
   validates :body, presence: true, length: { minumum: 10, maximum: 20_000 }
 end
